508caee8dace554416451d6f0b00aa9827983db4
root <root@GCserv01>
2016-12-15 22:36:11 +0100

    first commit

---
+++ src/test/java/selenium/TestPOC.java
@@ -0,0 +1,82 @@
+package selenium;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Created by Nicochu on 14/12/2016.
+ */
+public class TestPOC {
+
+    static WebDriver driver;
+    private static ChromeDriverService service;
+    static Wait<WebDriver> wait;
+
+	/*
+	
+    @BeforeClass
+    public static void createAndStartService() {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("/usr/chromedriver/chromedriver"))
+                .usingPort(12778)
+                .build();
+        try {
+            service.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+	
+	*/
+
+    @Before
+    public void createDriver() {
+        System.setProperty("webdriver.gecko.driver", "/usr/firefoxdriver/geckodriver");
+        driver = new FirefoxDriver();
+
+        /*
+        driver = new RemoteWebDriver(service.getUrl(),
+                DesiredCapabilities.chrome());
+                */
+    }
+
+
+
+    @Test
+    public void testHibernateJSP() {
+        
+        wait = new WebDriverWait(driver, 30);
+        driver.get("http://www.google.fr");
+
+        System.out.println(driver.findElement(By.xpath("//div")).toString());
+
+        try {
+            driver.findElement(By.xpath("//div"));
+        }
+        catch (NoSuchElementException e) {
+            assertTrue(false);
+        }
+        assertTrue(true);
+    }
+
+    @After
+    public void killFirefox() {
+        driver.quit();
+    }
+}
